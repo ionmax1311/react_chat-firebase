4468f004926211a5cdf98cb62bef017a1bc69925
Пользователь <ionmax1311@gmail.com>
2020-08-07 22:26:55 +0300

    first commit

---
+++ build/precache-manifest.c760603d5037ee5fbc763f7a23dd33a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c84eef36bbdc663f702b72a685ea5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc467544b65fecf0d3a0",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "eee23cd8f0c48144bc35",
+    "url": "/static/js/2.bdd3a2e1.chunk.js"
+  },
+  {
+    "revision": "5e5c05cd64be2171836370877cec97fe",
+    "url": "/static/js/2.bdd3a2e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc467544b65fecf0d3a0",
+    "url": "/static/js/main.cdbbfb06.chunk.js"
+  },
+  {
+    "revision": "27d7509e7747312a17a8",
+    "url": "/static/js/runtime-main.31f80f88.js"
+  }
+]);
